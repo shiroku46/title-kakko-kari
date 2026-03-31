262b99e67177092d4147ba765f30ec8cc6b1f5f6
shiroku46 <46mastei@gmail.com>
2026-03-31 21:48:02 +0900

    update server URL to Render

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@
 // 実機（Expo Go）でテスト: http://<PCのローカルIP>:3000
 //   例: http://192.168.1.10:3000
 //   PCのIPは「ipconfig」コマンドで確認できます
-export const DEFAULT_SERVER_URL = 'http://localhost:3000';
+export const DEFAULT_SERVER_URL = 'https://title-kakko-kari.onrender.com';
